93c9615f153ec230e0c62ffcc4d939f707739d0a
Sebastian Wagner <swagner@intevation.de>
2026-03-26 21:15:12 +0100

    lowercase script: also convert status and tags
    and use lower() function also for the uppercase-detection

--- scripts/emails_to_lowercase.py
+++ scripts/emails_to_lowercase.py
@@ -2,7 +2,14 @@
 """
 Convert contact email addresses to lowercase
 
-In the tables contact_automatic and contact, convert all values in the email column to lowercase.
+In these tables convert all email address values in the email column to lowercase:
+- contact_automatic
+- contact
+- email_status
+- email_tag
+
+Not necessary:
+- email_annotation: That's a view
 """
 import psycopg2
 
@@ -11,9 +18,9 @@ def main():
     conn = psycopg2.connect(dsn=read_configuration()["libpg conninfo"])
     try:
         with conn:
-            for table in ('contact_automatic', 'contact'):
+            for table in ('contact_automatic', 'contact', 'email_status', 'email_tag'):
                 with conn.cursor() as cur:
-                    cur.execute(f"UPDATE {table} SET email = lower(email) WHERE email ~ '[A-Z]'")
+                    cur.execute(f"UPDATE {table} SET email = LOWER(email) WHERE email != LOWER(email)'")
     finally:
         # Have to manually close connection even when using with-statement:
         # https://www.psycopg.org/docs/usage.html#with-statement
